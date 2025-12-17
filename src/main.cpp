6c4600f9c0ece5746aef3988a11a98a848019b50
rzrdt8 <giulio.ricci@aptiv.com>
2025-12-17 11:22:04 -0500

    change cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
 
 #include <iostream>
+#include <fstream>
+#include <string>
+
 int main() {
-    std::cout << "Hello from CI/CD change #1 !" << std::endl;
+    std::string filename = "output.txt";
+    std::ofstream outFile(filename);
+
+    if (!outFile) {
+        std::cerr << "Error: Could not open file for writing." << std::endl;
+        return 1;
+    }
+
+    outFile << "Hello, this is a test message written to the file!" << std::endl;
+    outFile << "You can add more lines as needed." << std::endl;
+
+    outFile.close();
+       std::cout << "Data written to " << filename << std::endl;
+
     return 0;
-}
+
+
